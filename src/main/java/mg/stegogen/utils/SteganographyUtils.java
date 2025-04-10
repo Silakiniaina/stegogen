e9046e74396394a27ba08312f42ce0bfce745319
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 16:12:40 +0300

    feat : Create function readInt.

--- src/main/java/mg/stegogen/utils/SteganographyUtils.java
+++ src/main/java/mg/stegogen/utils/SteganographyUtils.java
@@ -4,5 +4,13 @@ public class SteganographyUtils {
     public static final int BITS_PER_BYTE = 8;
     public static final String END_MARKER = "1111111111111111";
 
-    
+    /* -------------------------------------------------------------------------- */
+    /*                                  Functions                                 */
+    /* -------------------------------------------------------------------------- */
+    public static int readInt(byte[] data, int offset) {
+        return ((data[offset] & 0xFF) << 24) |
+                ((data[offset + 1] & 0xFF) << 16) |
+                ((data[offset + 2] & 0xFF) << 8) |
+                (data[offset + 3] & 0xFF);
+    }
 }

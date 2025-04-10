6dc010b97c3bd57c4d1759cb8ef6e08f5005ceaa
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 16:04:43 +0300

    feat : Create constructor for ImageSteganography.

--- src/main/java/mg/stegogen/image/ImageSteganography.java
+++ src/main/java/mg/stegogen/image/ImageSteganography.java
@@ -7,5 +7,12 @@ public class ImageSteganography {
     private static final byte[] PNG_SIGNATURE = {(byte) 0x89, 'P', 'N', 'G', '\r', '\n', '\u001a', '\n'};
     private RandomGenerator randomGenerator;
 
+    /* -------------------------------------------------------------------------- */
+    /*                                 Constructor                                */
+    /* -------------------------------------------------------------------------- */
+    public ImageSteganography(long seed) {
+        this.randomGenerator = new RandomGenerator(seed);
+    }
+
 }
 

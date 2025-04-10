a52f9532f7ea07cf24e3caf9cfac96a520197914
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 16:08:29 +0300

    feat : Create function readFile.

--- src/main/java/mg/stegogen/image/ImageSteganography.java
+++ src/main/java/mg/stegogen/image/ImageSteganography.java
@@ -1,18 +1,35 @@
 package mg.stegogen.image;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 import mg.stegogen.core.RandomGenerator;
 
 public class ImageSteganography {
 
-    private static final byte[] PNG_SIGNATURE = {(byte) 0x89, 'P', 'N', 'G', '\r', '\n', '\u001a', '\n'};
+    private static final byte[] PNG_SIGNATURE = { (byte) 0x89, 'P', 'N', 'G', '\r', '\n', '\u001a', '\n' };
     private RandomGenerator randomGenerator;
 
     /* -------------------------------------------------------------------------- */
-    /*                                 Constructor                                */
+    /* Constructor */
     /* -------------------------------------------------------------------------- */
     public ImageSteganography(long seed) {
         this.randomGenerator = new RandomGenerator(seed);
     }
 
-}
+    /* -------------------------------------------------------------------------- */
+    /* Functions */
+    /* -------------------------------------------------------------------------- */
+    private byte[] readFile(String filePath) throws IOException {
+        File inputFile = new File(filePath);
+        byte[] fileData = new byte[(int) inputFile.length()];
 
+        try (FileInputStream fis = new FileInputStream(inputFile)) {
+            fis.read(fileData);
+        }
+
+        return fileData;
+    }
+
+}

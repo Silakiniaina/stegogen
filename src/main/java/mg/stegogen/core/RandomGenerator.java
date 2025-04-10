1c9982686fd58163ed5132bc6e81546b8829d5fc
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 15:04:11 +0300

    feat : Create class RandomGenerator.

---
+++ src/main/java/mg/stegogen/core/RandomGenerator.java
@@ -0,0 +1,10 @@
+package mg.stegogen.core;
+
+public class RandomGenerator {
+    private long seed;
+    private long multiplier;
+    private long increment;
+    private long modulus;
+    private long currentValue;
+    
+}

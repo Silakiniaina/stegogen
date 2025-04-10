f372faf44e89b6484104290d21402831994e382b
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 16:40:20 +0300

    feat : Create class AudioSteganography.

---
+++ src/main/java/mg/stegogen/audio/AudioSteganography.java
@@ -0,0 +1,11 @@
+package mg.stegogen.audio;
+
+import mg.stegogen.core.RandomGenerator;
+
+public class AudioSteganography {
+    private static final int BITS_PER_BYTE = 8;
+
+    private RandomGenerator randomGenerator;
+
+}
+

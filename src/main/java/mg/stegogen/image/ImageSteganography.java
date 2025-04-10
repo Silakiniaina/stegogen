5c1ecbf41a2a09f50b17927b8bd0d04b59c96f2f
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 16:02:58 +0300

    feat : Create class ImageSteganography.

---
+++ src/main/java/mg/stegogen/image/ImageSteganography.java
@@ -0,0 +1,11 @@
+package mg.stegogen.image;
+
+import mg.stegogen.core.RandomGenerator;
+
+public class ImageSteganography {
+
+    private static final byte[] PNG_SIGNATURE = {(byte) 0x89, 'P', 'N', 'G', '\r', '\n', '\u001a', '\n'};
+    private RandomGenerator randomGenerator;
+
+}
+

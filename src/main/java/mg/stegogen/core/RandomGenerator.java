0accd56b13a4570e045353e507fb81c797ed52cf
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 15:05:11 +0300

    feat : Create getters and setters for RandomGenerator.

--- src/main/java/mg/stegogen/core/RandomGenerator.java
+++ src/main/java/mg/stegogen/core/RandomGenerator.java
@@ -7,4 +7,42 @@ public class RandomGenerator {
     private long modulus;
     private long currentValue;
     
+    /* -------------------------------------------------------------------------- */
+    /*                                   Getters                                  */
+    /* -------------------------------------------------------------------------- */
+    public long getSeed() {
+        return seed;
+    }
+    public long getMultiplier() {
+        return multiplier;
+    }
+    public long getIncrement() {
+        return increment;
+    }
+    public long getModulus() {
+        return modulus;
+    }
+    public long getCurrentValue() {
+        return currentValue;
+    }
+    
+    /* -------------------------------------------------------------------------- */
+    /*                                   Setters                                  */
+    /* -------------------------------------------------------------------------- */
+    public void setSeed(long seed) {
+        this.seed = seed;
+    }
+    public void setMultiplier(long multiplier) {
+        this.multiplier = multiplier;
+    }
+    public void setIncrement(long increment) {
+        this.increment = increment;
+    }
+    public void setModulus(long modulus) {
+        this.modulus = modulus;
+    }
+    public void setCurrentValue(long currentValue) {
+        this.currentValue = currentValue;
+    }
+    
 }

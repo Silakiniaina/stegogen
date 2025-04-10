701267cede9e9bd41f7bcdbc7f36f96f733f6178
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 16:42:39 +0300

    feat : Create function validateWavFormat.

--- src/main/java/mg/stegogen/audio/AudioSteganography.java
+++ src/main/java/mg/stegogen/audio/AudioSteganography.java
@@ -14,5 +14,16 @@ public class AudioSteganography {
         this.randomGenerator = new RandomGenerator(seed);
     }
 
+    /* -------------------------------------------------------------------------- */
+    /*                                  Functions                                 */
+    /* -------------------------------------------------------------------------- */
+    private void validateWavFormat(byte[] audioData) {
+        if (audioData.length < 44 ||
+                audioData[0] != 'R' || audioData[1] != 'I' || audioData[2] != 'F' || audioData[3] != 'F' ||
+                audioData[8] != 'W' || audioData[9] != 'A' || audioData[10] != 'V' || audioData[11] != 'E') {
+            throw new IllegalArgumentException("Not a valid WAV file");
+        }
+    }
+
 }
 

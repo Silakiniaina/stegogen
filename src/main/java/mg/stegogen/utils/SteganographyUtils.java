4291aed85775108e943f60bc4b31805b638f3abc
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-10 16:11:51 +0300

    feat : Create class SteganographyUtils.

---
+++ src/main/java/mg/stegogen/utils/SteganographyUtils.java
@@ -0,0 +1,8 @@
+package mg.stegogen.utils;
+
+public class SteganographyUtils {
+    public static final int BITS_PER_BYTE = 8;
+    public static final String END_MARKER = "1111111111111111";
+
+    
+}

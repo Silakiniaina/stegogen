1740ca4c8c57783ab0badf723526869e07dd9b72
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-11 07:38:23 +0300

    feat : Add constructor for BaseSteganography

--- src/main/java/mg/stegogen/gui/BaseSteganography.java
+++ src/main/java/mg/stegogen/gui/BaseSteganography.java
@@ -1,10 +1,17 @@
 package mg.stegogen.gui;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 
 import mg.stegogen.core.RandomGenerator;
 
 public abstract class BaseSteganography {
     protected final RandomGenerator randomGenerator;
 
+    /* -------------------------------------------------------------------------- */
+    /*                                 Constructor                                */
+    /* -------------------------------------------------------------------------- */
+    public BaseSteganography(long seed) {
+        this.randomGenerator = new RandomGenerator(seed);
+    }
 }

49e57b18c34fd669d99980ba3accecfe46ffc729
Silakiniaina <sandasilakiniaina4@gmail.com>
2025-04-11 07:37:56 +0300

    feat : Create class BaseSteganography

---
+++ src/main/java/mg/stegogen/gui/BaseSteganography.java
@@ -0,0 +1,10 @@
+package mg.stegogen.gui;
+
+import java.io.IOException;
+
+import mg.stegogen.core.RandomGenerator;
+
+public abstract class BaseSteganography {
+    protected final RandomGenerator randomGenerator;
+
+}
